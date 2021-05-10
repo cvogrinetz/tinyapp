2b6db9b7d7d10d76de8d7c9a6a0b7310a1e9671c
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-10 05:44:49 +0000

    Add get request for /urls.json and make it send back urlDatabase

--- express_server.js
+++ express_server.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
 });
 
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
